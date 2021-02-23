317dc4303f8832fd7e0bdb522b7bd0cf719ff7d7
JohnCowNut <thuonghamno3xz@gmail.com>
2021-02-23 09:34:50 +0700

    fixed bug at Array FANNG and add code force daily

--- Interview-Pratice-FANNG/Array/container-with-most-water.ts
+++ Interview-Pratice-FANNG/Array/container-with-most-water.ts
@@ -17,4 +17,27 @@ function badSolution(numbers: number[]): number {
     }
     return maxArea
 }
-console.log(badSolution(test_case))
+// Time : O(n^2)
+// Space: O(1)
+//console.log(badSolution(test_case))
+
+function goodSolution(numbers: number[]): number {
+    let maxArea = 0;
+    let i = 0
+    let j = numbers.length - 1;
+    while (i != j) {
+        const area = Math.min(numbers[i], numbers[j]) * Math.abs(i - j)
+        console.log(numbers[i], numbers[j], area)
+        maxArea = Math.max(area, maxArea)
+        if (numbers[i] < numbers[j]) {
+            i++
+        } else {
+            j--
+        }
+    }
+
+    return maxArea
+}
+
+// Two pointer strategies
+console.log(goodSolution([1, 4, 5, 9]))
