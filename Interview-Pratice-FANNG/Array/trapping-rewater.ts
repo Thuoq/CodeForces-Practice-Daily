6d2942cf309a3ad913f719f7254d14bb579d6ec8
JohnCowNut <thuonghamno3xz@gmail.com>
2021-02-26 14:19:38 +0700

    add chat room solution and add question for FANNG at Array

---
+++ Interview-Pratice-FANNG/Array/trapping-rewater.ts
@@ -0,0 +1,18 @@
+/**
+ * Give an array of integer representing an elevation map
+ * where the width of each bar is 1, return how much rainwater
+ * can be trapped
+ */
+
+
+// function firstSolution(numbers: number[]): number {
+
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i + 1] == 0) {
+
+//         }
+//     }
+
+//     return 0
+// }
