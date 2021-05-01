cd7225e7e1c23fb331d1ddc8cfcdbf8facc53909
JohnCowNut <thuonghamno3xz@gmail.com>
2021-05-01 10:10:28 +0700

    Add A.Insomniacure solution

---
+++ Code-Force-Daily/C++Code/A.Insomniacure.cpp
@@ -0,0 +1,15 @@
+// Link problem: https://codeforces.com/problemset/problem/148/A
+#include <iostream>
+using  namespace std;
+
+int main() {
+    int k,l,m,n,d;
+    cin >> k >> l >> m >> n >>d;
+    int ans = 0;
+    for(int i = 1 ; i <= d ; i ++) {
+        if(i % k == 0 || i % l  == 0 || i % m == 0 || i % n == 0) {
+            ans ++;
+        }
+    }
+    cout << ans;
+}
