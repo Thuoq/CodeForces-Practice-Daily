72d264873ec37e47b57b10df243eccc0975e41f7
JohnCowNut <thuonghamno3xz@gmail.com>
2021-05-06 23:58:29 +0700

    edit file  settings.json and add soluition A.Polyhedrons

--- Code-Force-Daily/C++Code/A.AntonandPolyhedrons.cpp
+++ Code-Force-Daily/C++Code/A.AntonandPolyhedrons.cpp
@@ -1,9 +1,25 @@
 // Link Problem: https://codeforces.com/problemset/problem/785/A
 #include<iostream>
+#include<string>
 #include<map>
 
 using namespace std;
 int main() {
-    int n ; 
-    cin >> n
+    int n ;
+    map<string, int> gquiz1;
+    gquiz1.insert(pair<string, int>("Tetrahedron",4));
+    gquiz1.insert(pair<string, int>("Cube",6));
+    gquiz1.insert(pair<string, int>("Octahedron", 8));
+    
+    gquiz1.insert(pair<string, int>("Dodecahedron", 12));
+    gquiz1.insert(pair<string, int>("Icosahedron", 20));
+    cin >> n;
+    int sum = 0;
+    while(n) {
+        string polyhedron;
+        cin >> polyhedron;
+        sum += gquiz1[polyhedron];
+        n--;
+    }
+    cout << sum;
 }
