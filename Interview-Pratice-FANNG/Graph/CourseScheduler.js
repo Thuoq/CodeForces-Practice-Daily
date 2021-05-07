a9e9a2ad6f57dce6a2a69ad15aee71d4ce30ea7f
JohnCowNut <thuonghamno3xz@gmail.com>
2021-05-07 13:16:37 +0700

    add solution topological for course scheduler

--- Interview-Pratice-FANNG/Graph/CourseScheduler.js
+++ Interview-Pratice-FANNG/Graph/CourseScheduler.js
@@ -44,7 +44,7 @@ var canFinish = function (numCourses, prerequisites) {
     }
     return true
 };
-console.log(canFinish(6,p))
+// console.log(canFinish(6,p))
 // Test 2
 
 // canFinish(7,
@@ -92,3 +92,43 @@ var canFinish = function (numCourses, prerequisites) {
 };
 
 // TOPOLOGICAL SORT
+var canFinishTopological = function (numCourses, prerequisites) {
+    const adjList = new Array(numCourses).fill(0).map(() => []);
+    let inDegree = new Array(numCourses).fill(0).map(() => 0);
+    for (let i = 0; i < prerequisites.length; i++) {
+        let connect = prerequisites[i];
+        inDegree[connect[0]] ++;
+        adjList[prerequisites[i][1]].push(prerequisites[i][0])
+    }
+    console.log(adjList)
+    console.log(inDegree)
+    while(inDegree.length) {
+        // find the number inDegree = 0
+        position=Infinity;
+        for(let i  = 0 ; i < inDegree.length ; i++) {
+            if (inDegree[i] == 0) {
+                position = i;
+                
+            }  
+        }
+        console.log('Here Position',position)
+     
+        if(position == Infinity){
+            return false
+        }
+        for(let i = 0 ; i < adjList[position].length; i++) {
+            // console.log(inDegree[adjList[position][i]])
+            // console.log(adjList[position][i])
+            if (inDegree[adjList[position][i]] !== 0) {
+                inDegree[adjList[position][i]] --;
+
+            }
+        }
+        console.log("InDegree sau khi tru",inDegree)
+        // inDegree.splice(position,1)
+        console.log('InDegree sau khi Splice',inDegree)
+    }
+    return true
+    
+};
+console.log(canFinishTopological(6, p))
