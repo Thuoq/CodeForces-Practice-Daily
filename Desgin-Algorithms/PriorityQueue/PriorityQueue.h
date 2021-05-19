70a99be8de6d345d61f8b8dc82bc818693f8f045
JohnCowNut <thuonghamno3xz@gmail.com>
2021-05-19 15:51:48 +0700

    git add Priority Queue class implementation by C++

--- Desgin-Algorithms/PriorityQueue/PriorityQueue.h
+++ Desgin-Algorithms/PriorityQueue/PriorityQueue.h
@@ -5,8 +5,8 @@ using namespace std;
 class PriorityQueue {
     private: 
         vector<int> heap;
-        bool comparator(int a, int b) {
-            return a > b;
+        bool comparator(int a,int b) {
+            return a> b;
         }
         int parent(int idx) {
             return floor((idx-1)/2);
@@ -17,9 +17,36 @@ class PriorityQueue {
         int right_child(int idx) {
             return idx* 2 + 2;
         }
+        bool compare(int i , int j){
+            return this->comparator(this->heap[i],this->heap[j]);
+        } 
+        void swap(int i , int j) {
+            int temp = this->heap[i];
+            this->heap[i] = this->heap[j];
+            this->heap[j] = temp;
+        }
+        void shiftUp() {
+            int nodeIndex = this->size()-1;
+            while(nodeIndex > 0 && this->compare(nodeIndex,this->parent(nodeIndex))) {
+                this->swap(nodeIndex,this->parent(nodeIndex));
+                nodeIndex = this->parent(nodeIndex);
+            }
+        }
+        void shiftDown() {
+            int nodeIdx = 0;
+            while((this->child_left(nodeIdx) < this->size() && this->compare(this->child_left(nodeIdx),nodeIdx)) ||
+            (this->right_child(nodeIdx) < this->size() && this->compare(this->right_child(nodeIdx),nodeIdx))) {
+                int generatorIdx = this->right_child(nodeIdx) < this->size() && this->compare(this->right_child(nodeIdx),nodeIdx)
+                ? this->right_child(nodeIdx)
+                : this->child_left(nodeIdx);
+                this->swap(generatorIdx,nodeIdx);
+                nodeIdx = generatorIdx;
+            }
+        }
 
     public:
-        PriorityQueue();
+        PriorityQueue(int) {
+        }
         int size() {
             return this->heap.size();
         }
@@ -28,6 +55,20 @@ class PriorityQueue {
         }
         bool isEmpty() {
             return this->heap.size() == 0;
+        }
+        int push(int val) {
+            this->heap.push_back(val);
+            this->shiftUp();
+            return this->size();
+        }
+        int pop() {
+            if(this->size() > 1) {
+                this->swap(0,this->size()-1);
+            }
+            int popVal = this->heap.front();
+            this->heap.erase(this->heap.begin());
+            return popVal;
+
         }
 
     
---
+++ Desgin-Algorithms/PriorityQueue/index.cpp
@@ -0,0 +1,4 @@
+#include "PriorityQueue.h"
+int main() {
+    cout << "Hello world";
+}
