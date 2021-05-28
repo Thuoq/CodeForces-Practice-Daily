978cdabfb7f4edf11f06ebfa0ffc5291db2463a0
JohnCowNut <thuonghamno3xz@gmail.com>
2021-05-28 14:18:53 +0700

    add solution subsetSum.py and Longest IncreaseSequence solution

--- Desgin-Algorithms/BackTracking/subsetSum.py
+++ Desgin-Algorithms/BackTracking/subsetSum.py
@@ -1,32 +1,29 @@
-def print_subset_sum(i, sol, psum, elements , x):
+def print_subset_binary(sol, elements):
+    no_elements = True
+    print('{', end='')
+    for i in range(0, len(sol)):
+        if sol[i] == 1:
+            if no_elements:
+                print(elements[i], sep='', end='')
+                no_elements = False
+            else:
+                print(', ', elements[i], sep='', end='')
+    print('}')
 
- # Base case
+def print_subset_sum(i, sol, psum, elements , x):
+    # Base case
     if psum == x:
         print_subset_binary(sol, elements )
-    elif i < len(elements ):
-    # Generate candidates
-        for k in range(0, 2):
-
-        # Check if recursion tree can be pruned
-            if psum + k * elements [i] <= x:
-
-                # Expand partial solution
-                sol[i] = k
-
-                # Update sum related to partial solution
-                psum = psum + k * elements[i]
-
-                # Try to expand partial solution
+    elif i < len(elements ):  
+        for k in range(0, 2):   
+            if psum + k * elements [i] <= x:               
+                sol[i] = k              
+                psum = psum + k * elements[i]           
                 print_subset_sum(i + 1, sol, psum, elements , x)
-
-                # not necessary:
-                #psum = psum - k*elements [i]
-
-                # Make sure a 0 indicates the absence of an element
                 sol[i] = 0
 
 
 def print_subset_sum_wrapper(elements , x):
     sol = [0] * (len(elements))
     print_subset_sum(0, sol, 0, elements , x)
-print_subset_sum_wrapper([8,6,7,5,3,10,9],15)
+print_subset_sum_wrapper([1,2,1],2)
--- Desgin-Algorithms/DynamicPrograming/LongestIncreaseSequence.cpp
+++ Desgin-Algorithms/DynamicPrograming/LongestIncreaseSequence.cpp
@@ -1,38 +1,39 @@
-// #include<iostream>
-// using namespace std;
-// int longestIncreaseSequenceRecursive(int arr[],int n,int &refMax) {
-//     // base case
-//     if(n == 0){
-//         return 1;
-//     }
-//     int maxTemp =0;
-//     for(int i = 0 ; i < n ; i ++) {
-//         int res = longestIncreaseSequenceRecursive(arr,i,refMax);
-//         if (arr[i - 1] < arr[n - 1] && res + 1 > maxTemp)
-//         {
-//             maxTemp = res + 1;
-//         }
-//     }
-//     if(refMax < maxTemp) {
-//         refMax = maxTemp;
-//     }   
+#include<iostream>
+#include<map>
+using namespace std;
+int longestIncreaseSequenceRecursive(int arr[],int n,int &refMax) {
+    if(n == 0){
+        return 1;
+    }
+    
+    int maxTemp = 1;
+    
+    for(int i = 1 ; i < n ; i ++) {
+        int res = longestIncreaseSequenceRecursive(arr,i,refMax);
+        if (arr[i - 1] < arr[n - 1] && res + 1 > maxTemp)
+        {
+            maxTemp = res + 1;
+        }
+    }
+    if(refMax < maxTemp) {
+        refMax = maxTemp;
+    }
+    return maxTemp;
+}
+int longestIncrease(int arr[],int n) {
+    int maxLength = 1;
+    longestIncreaseSequenceRecursive(arr,n,maxLength);
+    return maxLength;
+}
 
-//     return refMax;
-// }
-// int longestIncrease(int arr[],int n) {
-//     int maxLength = 1;
-//     longestIncreaseSequenceRecursive(arr,n,maxLength);
-//     return maxLength;
-// }
-
-// int main (){
-//     int n;
-//     cin >> n;
-//     int numbers[n];
-//     for(int i = 0 ; i < n ; i ++) {
-//         int _ ;
-//         cin >> _;
-//         numbers[i] =_;
-//     }
-//     cout << longestIncreaseSequenceIter(numbers,n);
-// }
+int main (){
+    int n;
+    cin >> n;
+    int numbers[n];
+    for(int i = 0 ; i < n ; i ++) {
+        int _ ;
+        cin >> _;
+        numbers[i] =_;
+    }
+    cout << longestIncrease(numbers, n);
+}
