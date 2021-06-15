a46cc55a7620c0b1efdb781203257546238b2bc7
JohnCowNut <thuonghamno3xz@gmail.com>
2021-06-15 14:44:16 +0700

    add isWorld solution cpp

--- Desgin-Algorithms/DynamicPrograming/isWord.cpp
+++ Desgin-Algorithms/DynamicPrograming/isWord.cpp
@@ -1,56 +1,77 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 using namespace std;
-class IsWordSolution {
-    private: 
-  
-        bool compareWithDict(string word)
+class IsWordSolution
+{
+private:
+    bool compareWithDict(string word)
+    {
+        // compare all word with dictionary
+        for (int i = 0; i < dictionaries.size(); i++)
         {
-            // compare all word with dictionary
-            for (int i = 0; i < dictionaries.size(); i++)
+            if (!dictionaries[i].compare(word))
             {
-                if (!dictionaries[i].compare(word))
-                {
-                    return true;
-                }
+                return true;
             }
-            return false;
         }
-        void readingSources(string fileName)
+        return false;
+    }
+    void readingSources(string fileName)
+    {
+        ifstream f(fileName);
+        string str;
+        while (getline(f, str))
         {
-            ifstream f(fileName);
-            string str;
-            while (getline(f, str))
+            dictionaries.push_back(str);
+        }
+    }
+    vector<string> dictionaries;
+
+public:
+    vector<string> result;
+    IsWordSolution()
+    {
+        // initialize constructor
+        // readfile
+        this->readingSources("1000word.txt");
+    }
+    bool dict(string word)
+    {
+        int size = word.size();
+        // base case
+        if (size == 0)
+            return true;
+        // subStr minust from i -> size-i ; and compare with dictionaries
+        for (int i = 1; i <= size; i++)
+        {
+            if (compareWithDict(word.substr(0, i)) && dict(word.substr(i, size - i)))
             {
-                dictionaries.push_back(str);
+                result.push_back(word.substr(0, i));
+                return true;
             }
         }
-    public:
-        vector<string> dictionaries;
-        IsWordSolution() {
-            // initialize constructor
-            // readfile
-            this->readingSources("1000word.txt");
-        }
-        bool dict(string word) {
-            int size= word.size();
-            // base case
-            if(size == 0) return true;
-            // subStr minust from i -> size-i ; and compare with dictionaries
-            for(int i = 1 ; i <= size ; i ++) {
-                if(compareWithDict(word.substr(0,i)) && dict(word.substr(i,size-i))) {
-                    cout << word.substr(0,i) << " ";
-                    return true;
-                }
-            }
-            return  false;
-        }
+        return false;
+    }
 };
-int main() {
+int main()
+{
+    int n;
+    cin >> n;
     IsWordSolution s1;
     string test_string;
     cin >> test_string;
-    s1.dict(test_string) ? cout << "\nYES" : cout <<  "NO";
+    if (s1.dict(test_string))
+    {
+        cout << "YES" << endl;
+        for (int i = s1.result.size(); i >= 0; i--)
+        {
+            cout << s1.result[i] << " ";
+        }
+    }
+    else
+    {
+        cout << "NO";
+    }
 }
