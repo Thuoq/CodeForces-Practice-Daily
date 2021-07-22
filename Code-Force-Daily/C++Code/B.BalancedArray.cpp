0e2b6012c480c7fc08cea8bd04157d06af49c193
JohnCowNut <thuonghamno3xz@gmail.com>
2021-07-22 14:36:33 +0700

    add solution B.BalancedArray.cpp

--- Code-Force-Daily/C++Code/B.BalancedArray.cpp
+++ Code-Force-Daily/C++Code/B.BalancedArray.cpp
@@ -8,16 +8,19 @@ void solution() {
     if( (n/2)%2 ) {
         cout << "NO" << endl;
     }else {
+        cout << "YES" << endl;
         for(int i = 2 ; i <= n/2 ; i+=2) {
             cout << n-i << " " << n+i << " ";
         }
         for(int i = 1 ; i < n/2 ; i +=2) {
-            cout << n-i << " " << n+i;
+            cout << n-i << " " << n+i << " ";
         }
         cout << "\n";
     } 
 }
 int main() {
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
     int test;
     cin >> test;
     while(test) {
