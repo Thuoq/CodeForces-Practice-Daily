3d4ebad390817d89d1f7158b872795757649a1a9
JohnCowNut <thuonghamno3xz@gmail.com>
2021-08-11 20:47:20 +0700

    add time check for doubleSum.py

--- RecursivePrograming/Chapter4.LinearRecursive/doubleSum.py
+++ RecursivePrograming/Chapter4.LinearRecursive/doubleSum.py
@@ -1,3 +1,5 @@
+import sys
+sys.setrecursionlimit(100000000)
 def double_sum_general(m,n):
     ans = 0
     for i in range(1,m+1):
@@ -7,8 +9,6 @@ def double_sum_general(m,n):
 
 
 # Time: O(m x n )
-print(double_sum_general(4,6))
-
 ## Recursive function 
 def inner_sum(m,n):
     if n <= 0:
@@ -18,5 +18,15 @@ def outer_sum(m ,n):
     if m <= 0:
         return 0
     return outer_sum(m-1,n) + inner_sum(m,n)
-
-print(outer_sum(4,6))
+from time import time
+t_0 = time()
+# For mode
+num_1 = double_sum_general(9000, 9000)
+t_1 = time()
+# Recursive mode
+num_2 = outer_sum(9000,9000)
+t_2 = time()
+print ('function for  takes %f' % (t_1-t_0))
+print('===========================')
+print ('function recursive takes %f' % (t_2-t_1))
+print(num_2,num_1)
