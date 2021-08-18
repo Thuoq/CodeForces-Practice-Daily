67a0ad25245f5a0787f519b8702edf3ac82aa706
JohnCowNut <thuonghamno3xz@gmail.com>
2021-08-18 15:44:07 +0700

    add solution CodeForce Dayly

---
+++ Code-Force-Daily/C++Code/A.OmkarandCompletion.cpp
@@ -0,0 +1,16 @@
+// Linkproblem:https://codeforces.com/problemset/problem/1372/A?fbclid=IwAR24M1kEX8JObRcPE_WJ7HPQfHSgZYXm6Kb1cKZk9ZaDmlDeqr-k6lWI4sk
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >>t;
+    while(t--) {
+        int n;
+        cin >> n;
+        for(int i = 0 ; i < n ; i ++) {
+            cout << 1 << " ";
+        }
+        cout << "\n";
+    }
+}
