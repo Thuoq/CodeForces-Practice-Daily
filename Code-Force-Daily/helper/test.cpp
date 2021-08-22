2908f13172135fbe929c138049b5aa5549a3ef3e
JohnCowNut <thuonghamno3xz@gmail.com>
2021-08-22 14:50:39 +0700

    add helper folder prepare for ACM

---
+++ Code-Force-Daily/helper/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char a = '9';
+    char b = '2';
+    cout << a + b << endl;
+    cout << a-'0' << " " << b -'0' << endl;
+    cout <<  (a + b)-'0'<< endl; 
+}
