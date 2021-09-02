ff294be06dbda18c1cae8e7178e1be5b8f754b2b
JohnCowNut <thuonghamno3xz@gmail.com>
2021-09-02 17:07:12 +0700

    add Cp3

---
+++ Competitives3/UVa00272-TEXQuotes.cpp
@@ -0,0 +1,25 @@
+// #include<bits/stdc++.h>
+
+// using namespace std;
+// class Solution {
+//     private:
+//         string s;
+//     public:
+//         Solution() {
+//             getline(cin,this->s);
+//         }
+//         string solve() {
+//             string ans = "";
+//             int tmp = 1;    
+
+//             for(int i =0 ; i < this->s.size() ; i ++) {
+//                 if(this->s[i] == "")
+//             }
+//             return ans;
+
+//         }
+// };
+// int main() {
+//     Solution p;
+//     p.solve();
+// }
