fad2e796c31584f5bf314b73b34395af7a443f5a
JohnCowNut <thuonghamno3xz@gmail.com>
2021-09-06 21:35:35 +0700

    change file name

---
+++ Antti LakeSon/Tree Algorithms/TreeUW.h
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+typedef vector<pair<int,int> > vi_p;
+typedef vector<int> vi;
+class TreeUndirectedWeight { 
+    private: 
+        int n;
+        vi_p; *adj;
+    public:
+        TreeUndirectedWeight(int n ) {
+            this->n  = n;
+            this->adj = new vi_p[n];
+        } 
+        /** Add Edges Connect node a  to node b and as well as node b to node a
+         * @param a is node a 
+         * @param b is node b
+         * @param w is weight from a to b
+        */
+        addEdges(int a, int b, int w) {
+            this->adj[a].push_back({make_pair(b,w)});
+            this->adj[b].push_back({make_pair(a,w)});       
+        }
+        /** BFS
+         * 
+        */
+        void bfs(int s ) {
+
+        }
+
+};
