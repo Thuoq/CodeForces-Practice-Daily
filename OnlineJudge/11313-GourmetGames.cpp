f2f3596bef3d250f6c175b16e1fca62bf84ffc74
JohnCowNut <thuonghamno3xz@gmail.com>
2021-09-12 17:37:25 +0700

    add template 11313

---
+++ OnlineJudge/11313-GourmetGames.cpp
@@ -0,0 +1,13 @@
+// Link problem: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=127&fbclid=IwAR3UM-2yLVWcjJdyZmcegTYYcLZVzxWrzG1NV71Ynj07aI54C09TN48BTRA
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n , m;
+        cin >> n >> m;
+
+    }
+}
