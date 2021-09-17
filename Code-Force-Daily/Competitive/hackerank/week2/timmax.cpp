81d6fd5cedf2a2a709579de22fb4c6fb1237910b
JohnCowNut <thuonghamno3xz@gmail.com>
2021-09-17 21:31:22 +0700

    add solution tim-max

--- Code-Force-Daily/Competitive/hackerank/week2/timmax.cpp
+++ Code-Force-Daily/Competitive/hackerank/week2/timmax.cpp
@@ -1,23 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef vector<pair<int,int> > vi_p;
+typedef long long int ll_i;
+typedef vector<pair<ll_i,ll_i> > vi_p;
+
 int main() {
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
     int t ;
     cin >> t;
     while(t--) {
         int n,k;
         cin >> n >> k;
         vi_p arr;
-        int idx_max = INT_MIN;
-        int val_max = INT_MIN;
+        // int idx_max = INT_MIN;
+        // int val_max = INT_MIN;
         for(int i = 0 ; i < n ; i ++) {
-            int _;
+            ll_i _;
             cin >> _;
             arr.push_back({i+1,_});
         }
-        int ans = INT_MIN;
+        ll_i ans = INT_MIN;
         for(int i = max(0,n-200) ;i  <n ; i++) {
             for(int j = i+1; j < n ; j ++) {
                 ans = max(arr[i].first * arr[j].first - k*(arr[i].second | arr[j].second),ans);
