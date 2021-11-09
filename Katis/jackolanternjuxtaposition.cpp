1df64d6a99c6739befde62c6789d79f03c94084a
JohnCowNut <thuonghamno3xz@gmail.com>
2021-11-09 09:23:21 +0700

    add solution Jackolantern on Katis

---
+++ Katis/jackolanternjuxtaposition.cpp
@@ -0,0 +1,8 @@
+// Link problem: https://open.kattis.com/problems/jackolanternjuxtaposition
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, b , c;
+    cin >> a >> b >> c; 
+    cout << a * b * c << endl;
+}
