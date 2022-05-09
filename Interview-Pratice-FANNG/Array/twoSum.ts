c005aa8250f09828f64dd0fc398698a818f2c4ba
Thuong Le <55220275+Thuoq@users.noreply.github.com>
2022-05-09 23:33:33 +0700

    modifile

--- Interview-Pratice-FANNG/Array/twoSum.ts
+++ Interview-Pratice-FANNG/Array/twoSum.ts
@@ -1,16 +1,17 @@
 // Given an array of integer, return the indices of the two numbers that add up to a given target
 //
+// console.log('a');
 function twoSum(numbers: number[], target: number) {
   const map = new Map<number, number>();
   for (let idx = 0; idx < numbers.length; idx++) {
-    if (map.get(numbers[idx])) {
-      return [idx, map.get(numbers[idx])];
+    if (map.has(numbers[idx])) {
+      return [map.get(numbers[idx]), idx];
     } else {
       const findNumber = target - numbers[idx];
       map.set(findNumber, idx);
     }
   }
-  return null;
+  return [];
 }
-const found = twoSum([1, 3, 7, 9, 2], 11);
+const found = twoSum([2, 7, 11, 15], 9);
 console.log(found);
